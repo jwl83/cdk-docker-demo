182c745912f2737202b6dd794c1de7c8fa08dca6
jwl83 <186691391+jwl83@users.noreply.github.com>
2025-07-24 08:28:00 +0100

    Experiment with using cdk-ecr-deployment to push to a dedicate ECR repo

--- src/cdk_docker_demo/hello.py
+++ src/cdk_docker_demo/hello.py
@@ -1,1 +1,2 @@
+# Force Docker image rebuild: 2025-07-24T08:03
 print("Hello world from CDK Docker Demo!")
--- stack/cdk_docker_demo_stack/cdk_docker_demo_stack.py
+++ stack/cdk_docker_demo_stack/cdk_docker_demo_stack.py
@@ -1,9 +1,12 @@
+import aws_cdk as cdk
 from aws_cdk import (
     Stack,
     aws_lambda as _lambda,
     aws_apigateway as _apigateway,
     aws_ecr_assets as _ecr_assets,
 )
+from aws_cdk.aws_ecr_assets import DockerImageAsset
+import cdk_ecr_deployment as ecrdeploy
 from constructs import Construct
 
 class CdkDockerDemoStack(Stack):
@@ -18,6 +21,26 @@ class CdkDockerDemoStack(Stack):
         #     code=_lambda.Code.from_asset('src/cdk_docker_demo'),
         #     handler='hello.handler',
         # )
+
+        # code_image = _lambda.DockerImageCode.from_image_asset(
+        #     directory="src",
+        #     cmd=["cdk_docker_demo.hello.handler"],
+        #     platform=_ecr_assets.Platform.LINUX_AMD64,
+        # ),
+
+        image = DockerImageAsset(self, "CDKDockerImageSrc",
+            directory="src",
+            # cmd=["cdk_docker_demo.hello.handler"],
+            platform=_ecr_assets.Platform.LINUX_AMD64,
+        )
+
+        # TODO: Create destination ECR repository if it doesn't exist
+
+        # Copy from cdk docker image asset to another ECR.
+        ecrdeploy.ECRDeployment(self, "CDKDockerImageDeployment",
+            src=ecrdeploy.DockerImageName(image.image_uri),
+            dest=ecrdeploy.DockerImageName(f"{cdk.Aws.ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/cdk-docker-demo:latest")
+        )
 
         hello_handler = _lambda.DockerImageFunction(
             self,
